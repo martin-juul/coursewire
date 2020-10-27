5c7bf0e02752e56dcc774386c93d1361af21ceca
Martin Juul <code@juul.xyz>
2020-10-27 09:05:47 +0100

    Remove fortify and jetstream as they're not needed for this project

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,6 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Fortify\TwoFactorAuthenticatable;
-use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 
 /**
@@ -49,9 +47,7 @@ class User extends Authenticatable
 {
     use HasApiTokens;
     use HasFactory;
-    use HasProfilePhoto;
     use Notifiable;
-    use TwoFactorAuthenticatable;
 
     protected $keyType = 'string';
     protected $dateFormat = 'Y-m-d H:i:sO';
