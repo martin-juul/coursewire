001d42d5727d426823e2741d98deafbfe6506481
Martin Juul <code@juul.xyz>
2020-10-28 11:55:58 +0100

    add driver for spaces

--- config/filesystems.php
+++ config/filesystems.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'cloud' => env('FILESYSTEM_CLOUD', 's3'),
+    'cloud' => env('FILESYSTEM_CLOUD', 'spaces'),
 
     /*
     |--------------------------------------------------------------------------
@@ -63,6 +63,21 @@ return [
             'bucket'   => env('AWS_BUCKET'),
             'url'      => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
+        ],
+
+        'spaces' => [
+            'driver'   => 's3',
+            'key'      => env('DO_SPACES_KEY'),
+            'secret'   => env('DO_SPACES_SECRET'),
+            'endpoint' => env('DO_SPACES_ENDPOINT'),
+            'region'   => env('DO_SPACES_REGION'),
+            'bucket'   => env('DO_SPACES_BUCKET'),
+
+            'cache' => [
+                'store'  => 'redis',
+                'expire' => 600,
+                'prefix' => 'cw_spaces_',
+            ],
         ],
 
     ],
