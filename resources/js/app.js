0c696b9cf8d9b9be591105f697233c988551bead
Martin Juul <code@juul.xyz>
2020-10-29 11:29:49 +0100

    Add sentry to the frontend

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,38 @@
 import Vue from 'vue';
 import vuetify from './plugins/vuetify';
+import * as Sentry from "@sentry/browser";
+import {Vue as VueIntegration} from "@sentry/integrations";
+import {Integrations} from "@sentry/tracing";
 
-Vue.config.devtools = true;
-Vue.config.performance = true;
+const sentryDsn = process.env.MIX_SENTRY_DSN;
+let tracesSampleRate = process.env.MIX_SENTRY_TRACES_SAMPLE_RATE;
+
+if (!tracesSampleRate || isNaN(tracesSampleRate)) {
+  tracesSampleRate = 0.1;
+}
+
+const isProduction = process.env.MIX_ENV === 'local' || process.env.MIX_ENV === 'staging';
+
+if (sentryDsn && sentryDsn !== '') {
+  Sentry.init({
+    dsn: sentryDsn,
+    integrations: [
+      new VueIntegration({
+        Vue,
+        tracing: true,
+        logErrors: isProduction,
+      }),
+      new Integrations.BrowserTracing(),
+    ],
+
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate,
+  });
+}
+
+Vue.config.devtools = isProduction;
+Vue.config.performance = isProduction;
 
 Vue.component('masthead', require('./components/masthead').default)
 Vue.component('impressum', require('./components/impressum').default)
