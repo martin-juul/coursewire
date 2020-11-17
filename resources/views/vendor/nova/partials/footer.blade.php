85915a1768fcabb2bbc463a3f8a14dd68af20892
Martin Juul <code@juul.xyz>
2020-11-17 13:57:24 +0100

    [dashboard/footer]: update copyright and link

--- resources/views/vendor/nova/partials/footer.blade.php
+++ resources/views/vendor/nova/partials/footer.blade.php
@@ -1,7 +1,6 @@
 <p class="mt-8 text-center text-xs text-80">
-    <a href="https://nova.laravel.com" class="text-primary dim no-underline">Laravel Nova</a>
+    <a href="https://juul.xyz/projects/coursewire" target="_blank" class="text-primary dim no-underline">CourseWire</a>
     <span class="px-1">&middot;</span>
-    &copy; {{ date('Y') }} Laravel LLC - By Taylor Otwell, David Hemphill, and Steve Schoger.
+    &copy; {{ date('Y') }} Martin Juul.
     <span class="px-1">&middot;</span>
-    v{{ \Laravel\Nova\Nova::version() }}
 </p>
