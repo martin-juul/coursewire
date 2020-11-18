5dafe9185c145bd98cb432f2d6f642413c146998
Martin Juul <code@juul.xyz>
2020-11-18 10:30:29 +0100

    add copyright.blade.php to be used in footers

---
+++ resources/views/partials/copyright.blade.php
@@ -0,0 +1,1 @@
+© {{ date('Y') }} <a class="cw-copyright-link" href="https://juul.xyz/projects/coursewire" target="_blank">{{ config('vendor.branding.name') }}</a> by {{ config('vendor.branding.author') }}. @lang('All rights reserved.')
--- resources/views/vendor/nova/partials/footer.blade.php
+++ resources/views/vendor/nova/partials/footer.blade.php
@@ -1,6 +1,5 @@
 <p class="mt-8 text-center text-xs text-80">
-    <a href="https://juul.xyz/projects/coursewire" target="_blank" class="text-primary dim no-underline">CourseWire</a>
     <span class="px-1">&middot;</span>
-    &copy; {{ date('Y') }} Martin Juul.
+    @include('partials.copyright')
     <span class="px-1">&middot;</span>
 </p>
